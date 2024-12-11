6adc1713b034b6d9f0ac737037bdeca27e0f605d
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-11 17:07:37 -0300

    docs: create screen of dinheiro emprestado

---
+++ financial/lib/screens/money.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Money extends StatelessWidget {
+  const Money({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Nesta pagina deram adicionado os dinheiros emprestados!'),
+      ),
+    );
+  }
+}
