25f413f9ad4089e5f4ebacf29b3d489f60e80b49
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-11 17:07:58 -0300

    docs: create screen of parcelado

---
+++ financial/lib/screens/parcelado.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Parcelado extends StatelessWidget {
+  const Parcelado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Nesta pagina deram adicionado as compras parceladas!'),
+      ),
+    );
+  }
+}
