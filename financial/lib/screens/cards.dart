f18265494ab34bc761aacd25d42b30b61c9c9db9
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-11 17:07:23 -0300

    docs: create screen of cards

---
+++ financial/lib/screens/cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Cards extends StatelessWidget {
+  const Cards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+            'Nesta pagina deram adicionado as compras parceladas no cartão!'),
+      ),
+    );
+  }
+}
