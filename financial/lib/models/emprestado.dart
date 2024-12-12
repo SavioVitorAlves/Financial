083f3527451df51ce9494a339d1b4e64b5749777
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-12 17:45:41 -0300

    docs: create models

---
+++ financial/lib/models/emprestado.dart
@@ -0,0 +1,7 @@
+class Emprestado {
+  final String descricao;
+  final double valor;
+  final DateTime date;
+  Emprestado(
+      {required this.descricao, required this.valor, required this.date});
+}
---
+++ financial/lib/models/pessoa.dart
@@ -0,0 +1,8 @@
+import 'package:financial/models/emprestado.dart';
+
+class Pessoa {
+  final String nome;
+  final List<Emprestado> dinheiro;
+
+  Pessoa({required this.nome, required this.dinheiro});
+}
