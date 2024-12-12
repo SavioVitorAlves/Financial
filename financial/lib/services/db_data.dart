664e7bf9931fa64dd41aad2cf26e69cd83c38c19
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-12 17:47:45 -0300

    feat: add fisrt codce

--- financial/lib/services/db_data.dart
+++ financial/lib/services/db_data.dart
@@ -1,1 +1,19 @@
-class DbData {}
+import 'package:financial/models/pessoa.dart';
+import 'package:financial/utils/db_util.dart';
+
+class DbData {
+  List<Pessoa> _pessoas = [];
+
+  List<Pessoa> get meses {
+    return [..._pessoas];
+  }
+
+  DbData([this._pessoas = const []]);
+
+  Future<void> loadPessoas() async {
+    final dbData = DbUtil();
+    final db = await dbData.database();
+
+    final result = await dbData.getPessoaComDinheiro(db);
+  }
+}
