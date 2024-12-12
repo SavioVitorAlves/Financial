d06747e1202df811b7548d9173ead81b4b122b4c
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-12 11:03:57 -0300

    feat: add base configuration

--- financial/lib/screens/money.dart
+++ financial/lib/screens/money.dart
@@ -5,9 +5,140 @@ class Money extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Dinheiro Emprestado',
+          style:
+              TextStyle(color: Color.fromARGB(255, 2, 19, 119), fontSize: 18),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
       body: Center(
-        child: Text('Nesta pagina deram adicionado os dinheiros emprestados!'),
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.monetization_on_outlined,
+                      size: 30,
+                      color: Color.fromARGB(255, 2, 19, 119),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '8,420.00',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 2, 19, 119),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    'Recebidos Recente',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 2, 19, 119),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 70,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(50, 158, 158, 158),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.blue,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'N',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Nome da Pessoa',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 2, 19, 119))),
+                          Text('R\$ 25.00',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 2, 19, 119))),
+                        ],
+                      ),
+                      const Spacer(),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.remove_circle,
+                            size: 35,
+                            color: Color.fromARGB(255, 2, 19, 119),
+                          ))
+                    ],
+                  ),
+                ),
+
+                const Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(300, 50),
+                    backgroundColor: const Color.fromARGB(255, 2, 19, 119),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Ação do botão
+                  },
+                  child: const Text('Adicionar Pessoa',
+                      style: TextStyle(color: Colors.white)),
+                ), // ,
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
