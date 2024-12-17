ee4425e516d7200ce16b892d8d4f725cc4600437
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-17 17:36:39 -0300

    feat: add provider dependence

--- financial/lib/main.dart
+++ financial/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:financial/screens/money.dart';
 import 'package:financial/screens/cards.dart';
 import 'package:financial/screens/homeInitial.dart';
 import 'package:financial/screens/parcelado.dart';
+import 'package:financial/services/db_data.dart';
 import 'package:financial/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,19 +17,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Financial',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => DbData(),
+      child: MaterialApp(
+        title: 'Financial',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        //home: const Homeinitial(),
+        routes: {
+          AppRoutes.HOME: (ctx) => const Homeinitial(),
+          AppRoutes.EMPRESTADO: (ctx) => const Money(),
+          AppRoutes.PARCELADO: (ctx) => const Parcelado(),
+          AppRoutes.CARTOES: (ctx) => const Cards(),
+        },
       ),
-      //home: const Homeinitial(),
-      routes: {
-        AppRoutes.HOME: (ctx) => const Homeinitial(),
-        AppRoutes.EMPRESTADO: (ctx) => const Money(),
-        AppRoutes.PARCELADO: (ctx) => const Parcelado(),
-        AppRoutes.CARTOES: (ctx) => const Cards(),
-      },
     );
   }
 }
