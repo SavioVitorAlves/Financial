4088226d2121c5356f4f7dc55a3296345ef5fc84
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-24 11:05:08 -0300

    docs: create model gastos

---
+++ financial/lib/models/gasto.dart
@@ -0,0 +1,11 @@
+class Gasto {
+  final int id;
+  final String descricao;
+  final double valor;
+  final DateTime date;
+  Gasto(
+      {required this.id,
+      required this.descricao,
+      required this.valor,
+      required this.date});
+}
