956a16b421f443a97a6f9909f0c6b059767cbb71
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-26 09:41:25 -0300

    docs: create model of credito

---
+++ financial/lib/models/credito.dart
@@ -0,0 +1,11 @@
+class Credito {
+  final int id;
+  final String descricao;
+  final double valor;
+  final DateTime date;
+  Credito(
+      {required this.id,
+      required this.descricao,
+      required this.valor,
+      required this.date});
+}
