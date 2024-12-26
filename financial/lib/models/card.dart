a04800dbcc75458e8eb905eb4c543485f4780b02
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-26 09:41:41 -0300

    refactor: refactor code model

--- financial/lib/models/card.dart
+++ financial/lib/models/card.dart
@@ -1,11 +1,11 @@
-import 'package:financial/models/emprestado.dart';
+import 'package:financial/models/credito.dart';
 
 class Card {
   final int id;
   final String name;
   final String name_pessoa;
   final String cor;
-  final List<Emprestado> credito;
+  final List<Credito> credito;
   Card(
       {required this.id,
       required this.name,
