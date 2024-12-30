a94434f48a39d2c2abdb7fb60448a2a1821a0a62
SavioVitorAlves <saviovitor2@gmail.com>
2024-12-30 16:45:55 -0300

    docs: create model extrato

---
+++ financial/lib/models/extrato.dart
@@ -0,0 +1,11 @@
+class Extrato {
+  final int id;
+  final String descricao;
+  final double valor;
+  final DateTime date;
+  Extrato(
+      {required this.id,
+      required this.descricao,
+      required this.valor,
+      required this.date});
+}
